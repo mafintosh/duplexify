f75b95ca600f80252fde342cc54e6723ff2438a2
Mathias Buus <mathiasbuus@gmail.com>
2014-07-07 18:46:33 +0200

    docs

---
+++ example.js
@@ -0,0 +1,20 @@
+var duplexify = require('duplexify')
+var http = require('http')
+
+var request = function(opts) {
+  var req = http.request(opts)
+  var dup = duplexify(req)
+  req.on('response', function(res) {
+    dup.setReadable(res)
+  })
+  return dup
+}
+
+var req = request({
+  method: 'GET',
+  host: 'www.google.com',
+  port: 80
+})
+
+req.end()
+req.pipe(process.stdout)
